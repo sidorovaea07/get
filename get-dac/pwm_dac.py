bfa4bd23c7ce09f79cb235fc9f1014c3feec3c65
sidorovaea07 <lenasidorova007@gmail.com>
2026-04-15 19:26:57 +0300

    3

--- get-dac/pwm_dac.py
+++ get-dac/pwm_dac.py
@@ -2,42 +2,42 @@ import RPi.GPIO as GPIO
 
 
 class PWM_DAC:
-    def __init__(self, gpio_bits, pwm_frequency, verbose=False):
-        self.gpio_bits = gpio_bits
-        self.pwm_frequency = pwm_frequency
+    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose=False):
+        self.gpio_pin = gpio_pin
+        self.dynamic_range = dynamic_range
         self.verbose = verbose
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_bits, GPIO.OUT, initial=0)
+        GPIO.setup(self.gpio_pin, GPIO.OUT)
+
+        self.pwm = GPIO.PWM(self.gpio_pin, pwm_frequency)
+        self.pwm.start(0)
 
     def deinit(self):
-        GPIO.output(self.gpio_bits, 0)
+        self.pwm.stop()
+        GPIO.output(self.gpio_pin, 0)
         GPIO.cleanup()
-
-    def set_number(self, number):
-        bits = [int(bit) for bit in bin(number)[2:].zfill(8)]
-        GPIO.output(self.gpio_bits, bits)
-
-        if self.verbose:
-            voltage = number / 255 * self.dynamic_range
-            print(f"Число на вход ЦАП: {number}, биты: {bits}")
-            #print(f"Расчётное напряжение: {voltage:.2f} В")
 
     def set_voltage(self, voltage):
         if not (0.0 <= voltage <= self.dynamic_range):
             if self.verbose:
                 print(f"Напряжение выходит за динамический диапазон (0.00 - {self.dynamic_range:.2f} В)")
-                #print("Устанавливаем 0.0 В")
-            number = 0
+                print("Устанавливаем 0.0 В")
+            duty_cycle = 0
         else:
-            number = int(voltage / self.dynamic_range * 255)
-            self.set_number(number)
+            duty_cycle = voltage / self.dynamic_range * 100
+
+        self.pwm.ChangeDutyCycle(duty_cycle)
+
+        if self.verbose:
+            print(f"Установлено напряжение: {voltage:.2f} В")
+            print(f"Коэффициент заполнения: {duty_cycle:.2f} %")
 
 
 if __name__ == "__main__":
     try:
         dac = PWM_DAC(12, 500, 3.290, True)
-        
+
         while True:
             try:
                 voltage = float(input("Введите напряжение в Вольтах: "))
