4362b6bb9c40d9dcba82e4ca51a2d3525507423c
sidoroea07 <lenasidorova007@gmail.com>
2026-04-15 13:50:50 +0300

    home upd

--- get-led/bin-counter.py
+++ get-led/bin-counter.py
@@ -4,43 +4,34 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 leds = [16, 12, 25, 17, 27, 23, 22, 24]
+UP_BTN = 5
+DOWN_BTN = 6
 
-for led in leds:
-    GPIO.setup(led, GPIO.OUT)
-    GPIO.output(led, 0)
+for pin in leds:
+    GPIO.setup(pin, GPIO.OUT)
 
-up = 12
-down = 19
+GPIO.output(leds, 0)
 
-GPIO.setup(up, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(down, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(UP_BTN, GPIO.IN)
+GPIO.setup(DOWN_BTN, GPIO.IN)
 
 num = 0
+sleep_time = 0.2
 
 def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
 
-sleep_time = 0.2
+while True:
+    if GPIO.input(UP_BTN):
+        num += 1
+        if num > 255:
+            num = 0
+        GPIO.output(leds, dec2bin(num))
+        time.sleep(sleep_time)
 
-try:
-    while True:
-        if GPIO.input(up) == 1:
-            num = num + 1
-            if num > 255:
-                num = 0
-            print(num, dec2bin(num))
-            GPIO.output(leds, dec2bin(num))
-            time.sleep(sleep_time)
-        
-        if GPIO.input(down) == 1:
-            num = num - 1
-            if num < 0:
-                num = 255
-            print(num, dec2bin(num))
-            GPIO.output(leds, dec2bin(num))
-            time.sleep(sleep_time)
-        
-        time.sleep(0.01)
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+    if GPIO.input(DOWN_BTN):
+        num -= 1
+        if num < 0:
+            num = 0
+        GPIO.output(leds, dec2bin(num))
+        time.sleep(sleep_time)
